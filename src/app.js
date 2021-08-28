bb631cd1cfa8ff1361f04906072fca3e6ac693f3
FiorellaCrocco <fiorellacrocco@gmail.com>
2021-08-28 02:23:36 +0000

    Mostrando en pantalla

--- src/app.js
+++ src/app.js
@@ -10,17 +10,26 @@ window.onload = function() {
   console.log("Hello Rigo!");
 };
 
-let pronoun = ["the", "our"];
-let adj = ["great", "big"];
-let noun = ["jogger", "racoon"];
-let dominio = [".com.uy", ".edu.uy"];
+let createTexto = () => {
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon"];
+  let dominio = [".com.uy", ".edu.uy"];
 
-for (let i = 0; i < pronoun.length; i++) {
-  for (let j = 0; j < adj.length; j++) {
-    for (let k = 0; k < noun.length; k++) {
-      for (let l = 0; l < dominio.length; l++) {
-        console.log(pronoun[i] + adj[j] + noun[k] + dominio[l]);
+  let ul = "";
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < dominio.length; l++) {
+          console.log(pronoun[i] + adj[j] + noun[k] + dominio[l]);
+          ul += "<h3>" + pronoun[i] + adj[j] + noun[k] + dominio[l] + "</h3>";
+        }
       }
     }
   }
-}
+  return ul;
+};
+
+document.getElementById("mostrar").innerHTML = createTexto();
+console.log(createTexto());
