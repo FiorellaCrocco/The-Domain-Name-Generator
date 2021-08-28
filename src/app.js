8ee94b1e52a73e3fa8804c16882ae8f5e4993f27
FiorellaCrocco <fiorellacrocco@gmail.com>
2021-08-28 00:46:22 +0000

    Terminado el pedido

--- src/app.js
+++ src/app.js
@@ -7,5 +7,20 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello Rigo!");
 };
+
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let dominio = [".com.uy", ".edu.uy"];
+
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let l = 0; l < dominio.length; l++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + dominio[l]);
+      }
+    }
+  }
+}
